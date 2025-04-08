f8b55ec1e63c93b2a2c1c76840f5ea7d40db9468
dciepiela <d.ciepiela00@gmail.com>
2025-04-08 21:07:54 +0200

    Flask example

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return jsonify({"message": "Witaj w moim API!"})
+@app.route('/api/v1.0/predict')
+def predict():
+    num1 = request.args.get('num1', type=float)
+    num2 = request.args.get('num2', type=float)
+
+    prediction = num1 + num2
+    label = "1" if prediction > 5.8 else "0"
+    return jsonify({"prediction": label, "features": {"num1": num1, "num2": num2}})
 
 if __name__ == '__main__':
     app.run()
